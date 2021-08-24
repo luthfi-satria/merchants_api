dc05acfd68cef8978e432f6553bbb16b870823f8
Nathaniel Gunawan <nielchvo@gmail.com>
2021-08-24 16:28:35 +0700

    Fix cannot update entity due to typeorm 'relations'

--- src/stores/stores-operational.controller.ts
+++ src/stores/stores-operational.controller.ts
@@ -89,6 +89,9 @@ export class StoreOperationalController {
             ...res,
             is_open_24h: is_open_24_hour,
           });
+          delete store.operational_hours;
+          delete store.service_addon;
+
           await this.mStoreService.updateStoreProfile(store).catch((e) => {
             throw e;
           });
