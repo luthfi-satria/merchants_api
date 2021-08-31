f166feacb1cf8fe2ce5f3a3dd2d001d3368093c9
Dery Mukti R <deryprogrammer@gmail.com>
2021-08-31 11:42:54 +0700

    add longitude latitude and distance

--- src/query/validation/query-public.dto.ts
+++ src/query/validation/query-public.dto.ts
@@ -1,8 +1,21 @@
 import { Type } from 'class-transformer';
-import { IsBoolean, IsNumber, IsOptional, IsUUID } from 'class-validator';
+import { IsBoolean, IsLatitude, IsLongitude, IsNotEmpty, IsNumber, IsOptional, IsUUID } from 'class-validator';
 
 export class QueryListStoreDto {
   search: string;
+  
+  @IsLongitude()
+  @IsNotEmpty()
+  location_longitude:string;
+
+  @IsLatitude()
+  @IsNotEmpty()
+  location_latitude:string;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  distance: number;
 
   @IsNumber()
   @IsOptional()
