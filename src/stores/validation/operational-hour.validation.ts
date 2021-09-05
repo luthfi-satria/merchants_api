b939884037330383fc7770e9112771a6ea81ae67
Nathaniel Gunawan <nielchvo@gmail.com>
2021-09-06 00:12:40 +0700

    Fix update store operational hour & shift schedules
    - change flow to support cascade update relational (shifts)
    from typeorm

--- src/stores/validation/operational-hour.validation.ts
+++ src/stores/validation/operational-hour.validation.ts
@@ -4,6 +4,7 @@ import {
   IsIn,
   IsMilitaryTime,
   IsNotEmpty,
+  IsNumber,
   IsOptional,
   IsString,
   IsUUID,
@@ -27,6 +28,10 @@ export class StoreShiftHours {
   @IsUUID()
   @IsOptional()
   id?: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  shift_id: number;
 
   @IsNotEmpty()
   @IsMilitaryTime({ each: true })
