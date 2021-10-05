5c37c20f51f5d40a84dbf4804e50a4cc1fb8ef94
Nathaniel Gunawan <nielchvo@gmail.com>
2021-10-05 15:10:10 +0700

    fix (store-bank-data): Fix validation payload for Update store bank detail

--- src/banks/validations/banks-store.dto.ts
+++ src/banks/validations/banks-store.dto.ts
@@ -1,6 +1,24 @@
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+} from 'class-validator';
+
 export class BanksStoreDto {
+  @IsArray()
+  @ArrayMinSize(1)
+  @IsString({ each: true })
   store_ids: string[];
+
+  @IsNotEmpty()
   bank_id: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
   bank_account_no: string;
+
+  @IsNotEmpty()
   bank_account_name: string;
 }
