3d749a381270e7dce67eba01f521e27a5cf14e31
Ronald Arie Bowo Supardi <ronald@akarinti.tech>
2021-10-21 16:26:52 +0700

    feat(store) : add price_category object in items

--- src/common/catalogs/catalogs.service.ts
+++ src/common/catalogs/catalogs.service.ts
@@ -5,12 +5,14 @@ import {
   InternalServerErrorException,
   Logger,
 } from '@nestjs/common';
-import { firstValueFrom, map } from 'rxjs';
+import { firstValueFrom, lastValueFrom, map } from 'rxjs';
 import { CommonService } from '../common.service';
 import { Message } from 'src/message/message.decorator';
 import { MessageService } from 'src/message/message.service';
 import { ResponseService } from 'src/response/response.service';
 import { Response } from 'src/response/response.decorator';
+import { AxiosResponse } from 'axios';
+import { PriceCategoryDTO } from './dto/price_category.dto';
 
 @Injectable()
 export class CatalogsService {
@@ -37,6 +39,49 @@ export class CatalogsService {
     } catch (e) {
       this.logger.error(
         `${process.env.BASEURL_CATALOGS_SERVICE}/api/v1/internal/menu/${id}`,
+      );
+      if (e.response) {
+        throw new HttpException(
+          e.response.data.message,
+          e.response.data.statusCode,
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async getPriceCategoryByMerchantIds(
+    merchantIds: string[],
+  ): Promise<PriceCategoryDTO[]> {
+    try {
+      if (!merchantIds) {
+        return null;
+      }
+      const params = {
+        merchant_ids: merchantIds,
+      };
+      const headerRequest = {
+        'Content-Type': 'application/json',
+      };
+      const url = `${process.env.BASEURL_CATALOGS_SERVICE}/api/v1/internal/catalogs/price-category`;
+      const post_request = this.httpService
+        .post(url, params, { headers: headerRequest })
+        .pipe(
+          map((axiosResponse: AxiosResponse) => {
+            return axiosResponse.data;
+          }),
+        );
+      const response: {
+        success: string;
+        message: string;
+        data: PriceCategoryDTO[];
+      } = await lastValueFrom(post_request);
+
+      return response.data;
+    } catch (e) {
+      this.logger.error(
+        `${process.env.BASEURL_CATALOGS_SERVICE}/api/v1/internal/catalogs/price-category`,
       );
       if (e.response) {
         throw new HttpException(
---
+++ src/common/catalogs/dto/price_category.dto.ts
@@ -0,0 +1,10 @@
+export class PriceCategoryDTO {
+  id: string;
+  name: string;
+  merchant_id: string;
+  status: string;
+  default_price_category_id: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
