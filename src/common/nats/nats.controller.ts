f72a0e7ade78fe85b84c7db5e4d5e8011836ee05
Ronald Arie Bowo Supardi <ronald@akarinti.tech>
2021-11-26 19:57:46 +0700

    feat(nats): make store close from nats

--- src/common/nats/nats.controller.ts
+++ src/common/nats/nats.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Logger } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import { MenuEfoodService } from 'src/menu_efood/menu_efood.service';
+import { StoreOperationalService } from 'src/stores/stores-operational.service';
 
 @Controller('')
 export class NatsController {
   logger = new Logger(NatsController.name);
-  constructor(private readonly menuEfoodService: MenuEfoodService) {}
+  constructor(
+    private readonly menuEfoodService: MenuEfoodService,
+    private readonly mStoreOperationalService: StoreOperationalService,
+  ) {}
 
   @EventPattern('catalogs.storeavailability.created')
   async saveMenuEfood(@Payload() data: any) {
@@ -24,4 +28,27 @@ export class NatsController {
     this.logger.log('catalogs.storeavailability.deleted');
     // this.logger.debug(data);
   }
+
+  @EventPattern('orders.order.cancelled_by_store.busy')
+  async cancelledByStoreBusy(@Payload() data: any) {
+    this.logger.log('orders.order.cancelled_by_store.busy');
+    await this.mStoreOperationalService
+      .updateStoreOpenStatus(data.store_id, false)
+      .catch((e) => {
+        this.logger.error(e);
+        throw e;
+      });
+  }
+
+  @EventPattern('orders.order.cancelled_by_store.other')
+  async cancelledByStoreOther(@Payload() data: any) {
+    this.logger.log('orders.order.cancelled_by_store.other');
+    await this.mStoreOperationalService
+      .updateStoreOpenStatus(data.store_id, false)
+      .catch((e) => {
+        this.logger.error(e);
+        throw e;
+      });
+  }
+
 }
