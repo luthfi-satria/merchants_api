ba1c4934b105e2678c3a0aff4a3519c1c82be9b7
Ronald Arie Bowo Supardi <ronald@akarinti.tech>
2021-12-20 12:26:24 +0700

    fix(nats): fixing event close store , update status just for is_store_open

--- src/common/nats/nats.controller.ts
+++ src/common/nats/nats.controller.ts
@@ -33,7 +33,7 @@ export class NatsController {
   async cancelledByStoreBusy(@Payload() data: any) {
     this.logger.log('orders.order.cancelled_by_store.busy');
     await this.mStoreOperationalService
-      .updateStoreOpenStatus(data.store_id, false)
+      .updateIsStoreOpenStatus(data.store_id, false)
       .catch((e) => {
         this.logger.error(e);
         throw e;
@@ -44,7 +44,7 @@ export class NatsController {
   async cancelledByStoreOther(@Payload() data: any) {
     this.logger.log('orders.order.cancelled_by_store.operational');
     await this.mStoreOperationalService
-      .updateStoreOpenStatus(data.store_id, false)
+      .updateIsStoreOpenStatus(data.store_id, false)
       .catch((e) => {
         this.logger.error(e);
         throw e;
