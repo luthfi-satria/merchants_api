c6e15b1d7c28a2a3eb3841af1a7cca4dd3865c7b
saniakarinti <sani.paranta@akarinti.tech>
2022-01-30 06:32:04 +0700

    fix(menu-online): add catch handle on menu-online event

--- src/menu_online/menu_online.service.ts
+++ src/menu_online/menu_online.service.ts
@@ -1,5 +1,5 @@
 import { MenuOnlineDocument } from '../database/entities/menu_online.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -13,33 +13,39 @@ export class MenuOnlineService {
   async natsCreateStoreAvailability(data: any) {
     console.log('event natsCreateStoreAvailability: ', data);
     if (data.menu_price.menu_sales_channel.platform == 'ONLINE') {
-      const menuOnline = await this.menuOnlineRepository.findOne({
-        where: {
-          store_id: data.store_id,
-          menu_id: data.menu_price.menu_menu.id,
-          menu_price_id: data.menu_price.id,
-        },
-      });
-      if (menuOnline) {
-        menuOnline.name = data.menu_price.menu_menu.name;
-        menuOnline.photo = data.menu_price.menu_menu.photo;
-        menuOnline.price = data.menu_price.price;
-        menuOnline.menu_store_id = data.id;
-        menuOnline.updated_at = new Date();
+      try {
+        const menuOnline = await this.menuOnlineRepository.findOne({
+          where: {
+            store_id: data.store_id,
+            menu_id: data.menu_price.menu_menu.id,
+            menu_price_id: data.menu_price.id,
+          },
+        });
+        if (menuOnline) {
+          menuOnline.name = data.menu_price.menu_menu.name;
+          menuOnline.photo = data.menu_price.menu_menu.photo;
+          menuOnline.price = data.menu_price.price;
+          menuOnline.menu_store_id = data.id;
+          menuOnline.updated_at = new Date();
 
-        await this.menuOnlineRepository.update(menuOnline.id, menuOnline);
-      } else {
-        const menuOnlineData: Partial<MenuOnlineDocument> = {
-          menu_store_id: data.id,
-          menu_price_id: data.menu_price.id,
-          menu_id: data.menu_price.menu_menu.id,
-          name: data.menu_price.menu_menu.name,
-          photo: data.menu_price.menu_menu.photo,
-          price: data.menu_price.price,
-          store_id: data.store_id,
-        };
+          await this.menuOnlineRepository.update(menuOnline.id, menuOnline);
+        } else {
+          const menuOnlineData: Partial<MenuOnlineDocument> = {
+            menu_store_id: data.id,
+            menu_price_id: data.menu_price.id,
+            menu_id: data.menu_price.menu_menu.id,
+            name: data.menu_price.menu_menu.name,
+            photo: data.menu_price.menu_menu.photo,
+            price: data.menu_price.price,
+            store_id: data.store_id,
+          };
 
-        await this.menuOnlineRepository.save(menuOnlineData);
+          await this.menuOnlineRepository.save(menuOnlineData);
+        }
+      } catch (e) {
+        const logger = new Logger();
+        logger.log(e, 'Catch Error :  ');
+        throw e;
       }
     }
   }
@@ -47,22 +53,28 @@ export class MenuOnlineService {
   async natsUpdateStoreAvailabilityy(data: any) {
     console.log('event natsUpdateStoreAvailabilityy: ', data);
     if (data.menu_price.menu_sales_channel.platform == 'ONLINE') {
-      const menuOnline = await this.menuOnlineRepository.findOne({
-        menu_store_id: data.id,
-      });
+      try {
+        const menuOnline = await this.menuOnlineRepository.findOne({
+          menu_store_id: data.id,
+        });
 
-      if (menuOnline) {
-        menuOnline.store_id = data.store_id;
-        menuOnline.menu_price_id = data.menu_price.id;
-        menuOnline.menu_id = data.menu_price.menu_menu.id;
-        menuOnline.name = data.menu_price.menu_menu.name;
-        menuOnline.photo = data.menu_price.menu_menu.photo;
-        menuOnline.price = data.menu_price.price;
-        menuOnline.updated_at = new Date();
+        if (menuOnline) {
+          menuOnline.store_id = data.store_id;
+          menuOnline.menu_price_id = data.menu_price.id;
+          menuOnline.menu_id = data.menu_price.menu_menu.id;
+          menuOnline.name = data.menu_price.menu_menu.name;
+          menuOnline.photo = data.menu_price.menu_menu.photo;
+          menuOnline.price = data.menu_price.price;
+          menuOnline.updated_at = new Date();
 
-        await this.menuOnlineRepository.update(menuOnline.id, menuOnline);
-      } else {
-        await this.natsCreateStoreAvailability(data);
+          await this.menuOnlineRepository.update(menuOnline.id, menuOnline);
+        } else {
+          await this.natsCreateStoreAvailability(data);
+        }
+      } catch (e) {
+        const logger = new Logger();
+        logger.log(e, 'Catch Error :  ');
+        throw e;
       }
     }
   }
