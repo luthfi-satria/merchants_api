73a98ecf0d2d70d42348852a6da25ea5d6e01e3e
pandu <pandu@softwareseni.com>
2022-02-10 12:38:03 +0700

    fix: query in get list store

--- src/query/query.service.ts
+++ src/query/query.service.ts
@@ -727,7 +727,7 @@ export class QueryService {
                 ${
                   // jika params 'is24hrs' is 'false' / tidak di define, query list store include dgn store yg buka 24jam
                   is24hrs == false
-                    ? `AND ((:currTime >= operational_shifts.open_hour AND :currTime < operational_shifts.close_hour) OR operational_hours.is_open_24h = :all24h)`
+                    ? `AND (((:currTime >= operational_shifts.open_hour AND :currTime < operational_shifts.close_hour) OR (operational_shifts.open_hour > operational_shifts.close_hour AND ((operational_shifts.open_hour > :currTime AND operational_shifts.close_hour > :currTime) OR (operational_shifts.open_hour < :currTime AND operational_shifts.close_hour < :currTime)) )) OR operational_hours.is_open_24h = :all24h)`
                     : ''
                 }`,
                 {
