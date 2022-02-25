e1189497841fbc67ee96fc3018fad6ace259c727
rodericuskrista <rodericus.krista@akarinti.tech>
2022-02-25 16:45:12 +0700

    fix: delete unsued file

--- src/groups/wordings/wording-link-format-for-sms.ts
+++
@@ -1,8 +0,0 @@
-export const wordingLinkFormatForSms = (
-  username: string,
-  verificationLink: string,
-): string => {
-  const message = `Hai, ${username}!\n\nUntuk melakukan verifikasi, Klik link berikut: ${verificationLink} .\nJANGAN BAGIKAN LINK TERSEBUT KE SIAPAPUN termasuk eFOOD.\nWASPADA PENIPUAN!
-  `;
-  return message;
-};
