c11eba18c5d4f75c69218b3f3326940c0a414e55
luthfisr <luthfi.ramadhan@edot.id>
2022-10-17 16:45:00 +0700

    EF-314: multipickups - find nearest stores

--- src/stores/stores-multipickup.controller.ts
+++ src/stores/stores-multipickup.controller.ts
@@ -1,14 +1,10 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { MessageService } from 'src/message/message.service';
 import { ResponseStatusCode } from 'src/response/response.decorator';
-import { ResponseService } from 'src/response/response.service';
 import { StoreMultipickupService } from './stores-multipickup.service';
 
 @Controller('api/v1/merchants')
-export class StoreMultipickups {
+export class StoreMultipickupsController {
   constructor(
-    private readonly messageService: MessageService,
-    private readonly responseService: ResponseService,
     private readonly storeMultipickupService: StoreMultipickupService,
   ) {}
 
@@ -20,11 +16,7 @@ export class StoreMultipickups {
         queryParam,
       );
 
-      return this.responseService.success(
-        true,
-        this.messageService.get('merchant.listmerchant.success'),
-        findStores,
-      );
+      return findStores;
     } catch (error) {
       console.log(error);
       throw error;
