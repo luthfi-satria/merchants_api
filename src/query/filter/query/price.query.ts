68d4c6bb81ef30bb931ae515bf82fd6d31626e79
Ronald Arie Bowo Supardi <ronald@akarinti.tech>
2022-10-26 05:57:32 +0700

    fix: fixing filtering by pricerange in get store

--- src/query/filter/query/price.query.ts
+++ src/query/filter/query/price.query.ts
@@ -1,30 +1,38 @@
 import { FilterQueryInterface } from '../helper/interface/filter-query.interface';
-import { SelectQueryBuilder } from 'typeorm';
+import { Brackets, SelectQueryBuilder } from 'typeorm';
 import { StoreDocument } from '../../../database/entities/store.entity';
+import { PriceRangeDocument } from 'src/database/entities/price_range.entity';
 
 export class PriceQuery implements FilterQueryInterface {
   constructor(
     public query: SelectQueryBuilder<StoreDocument>,
     public moduleName: string,
     public isFilterPrice: boolean,
-    public priceLow: any,
-    public priceHigh: any,
+    public priceRangeFilter: PriceRangeDocument[],
   ) {}
 
   public getQuery(): SelectQueryBuilder<any> {
-    if (!this.isFilterPrice) {
+    if (!this.priceRangeFilter || this.priceRangeFilter.length == 0) {
       return this.query;
     }
 
-    this.query.andWhere(`${this.moduleName}.price >= :priceLow`, {
-      priceLow: this.priceLow,
-    });
-
-    if (!this.priceHigh.includes(0)) {
-      this.query.andWhere(`${this.moduleName}.price <= :priceHigh`, {
-        priceHigh: this.priceHigh,
-      });
-    }
+    this.query.andWhere(
+      new Brackets((qb) => {
+        // qb.where(
+        //   `( ${this.moduleName}.average_price >= :priceLow AND ${this.moduleName}.average_price >= :priceHigh)`,
+        //   {
+        //     priceLow: this.priceRangeFilter[0].price_low,
+        //     priceHigh: this.priceRangeFilter[0].price_high,
+        //   },
+        // );
+        for (let i = 0; i < this.priceRangeFilter.length; i++) {
+          const priceRange = this.priceRangeFilter[i];
+          qb.orWhere(
+            `( ${this.moduleName}.average_price >= ${priceRange.price_low} AND ${this.moduleName}.average_price >= ${priceRange.price_high})`,
+          );
+        }
+      }),
+    );
 
     return this.query;
   }
