bb3c77a9385ef9d9f5289bd2e2280026c0c04883
luthfisr <luthfi.ramadhan@edot.id>
2023-02-09 09:00:13 +0700

    add settings variable

--- src/database/seeders/settings/settings.data.ts
+++ src/database/seeders/settings/settings.data.ts
@@ -7,4 +7,24 @@ export const settings: { name: string; value: string }[] = [
     name: 'store_radius',
     value: '25',
   },
+  {
+    name: 'sso_process',
+    value: '0',
+  },
+  {
+    name: 'sso_timespan',
+    value: '60',
+  },
+  {
+    name: 'sso_data_limit',
+    value: '10',
+  },
+  {
+    name: 'sso_refresh_config',
+    value: '60',
+  },
+  {
+    name: 'sso_lastupdate',
+    value: null,
+  },
 ];
